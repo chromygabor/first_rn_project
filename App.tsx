191df1543609db1f5cf89f1171c3d2d6a5233bfa
Gabor Chromy <chromygabor@gmail.com>
2023-04-23 15:32:56 +0200

    ...

--- App.tsx
+++ App.tsx
@@ -1,16 +1,11 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {Dimensions} from 'react-native';
 import {Provider as PaperProvider} from 'react-native-paper';
-import {RootNavigator} from './src/RootNavigator';
+import Home from './src/screens/Home';
 
 function App(): JSX.Element {
   return (
     <PaperProvider>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
+      <Home />
     </PaperProvider>
   );
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,13 +1,55 @@
 import {Text, View} from 'react-native';
 import Styles from '../Styles';
+import {useState} from 'react';
+import {BottomNavigation} from 'react-native-paper';
 
 export interface IHomeProps {}
 
+const MusicRoute = () => <Text>Music</Text>;
+
+const AlbumsRoute = () => <Text>Albums</Text>;
+
+const RecentsRoute = () => <Text>Recents</Text>;
+
+const NotificationsRoute = () => <Text>Notifications</Text>;
+
 const Home: React.FC<IHomeProps> = (_props: IHomeProps) => {
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    {
+      key: 'music',
+      title: 'Favorites',
+      focusedIcon: 'heart',
+      unfocusedIcon: 'heart-outline',
+    },
+    {key: 'albums', title: 'Albums', focusedIcon: 'cart'},
+    {key: 'recents', title: 'Recents', focusedIcon: 'history'},
+    {
+      key: 'notifications',
+      title: 'Notifications',
+      focusedIcon: 'bell',
+      unfocusedIcon: 'bell-outline',
+    },
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    music: MusicRoute,
+    albums: AlbumsRoute,
+    recents: RecentsRoute,
+    notifications: NotificationsRoute,
+  });
+
   return (
-    <View style={Styles.Home}>
-      <Text>Home Screen</Text>
-    </View>
+    <>
+      <View style={Styles.Home}>
+        <Text>Home Screen</Text>
+      </View>
+      <BottomNavigation
+        navigationState={{index, routes}}
+        onIndexChange={setIndex}
+        renderScene={renderScene}
+      />
+    </>
   );
 };
 
